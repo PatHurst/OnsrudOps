f5dfa954bc61b3274d7684d0c6920beef5d675a0
Patrick Hurst <pathurst05@gmail.com>
2025-10-21 07:13:17 -0400

    Added SerialWrapper Test

--- Tests/OnsrudOps.Serial.Tests/SerialWrapperTests.cs
+++ Tests/OnsrudOps.Serial.Tests/SerialWrapperTests.cs
@@ -1,13 +1,26 @@
-﻿namespace OnsrudOps.Serial.Tests;
+﻿using System.IO.Ports;
+using System.Threading.Tasks;
+
+namespace OnsrudOps.Serial.Tests;
 
 [TestClass]
 public sealed class SerialWrapperTests
 {
     [TestMethod]
-    public void AssertConnectionReturnsFalseWhenFailed()
+    public void AssertConnectionMethodResultReturnMatchesProperty()
     {
         SerialWrapper serialWrapper = new();
         bool connectionSucceeded = serialWrapper.ConnectAsync().Result;
         Assert.IsTrue(connectionSucceeded == serialWrapper.IsConnected);
+    }
+
+    [TestMethod]
+    public async Task ConfigurationArgumentMatchesClassProperty()
+    {
+        SerialConnectionConfiguration config = new("COM3", 110, 7, StopBits.None, Parity.None);
+        SerialWrapper serialWrapper = new(config);
+        Assert.AreEqual(serialWrapper.Configuration, config);
+        await serialWrapper.ConnectAsync(SerialConnectionConfiguration.Default);
+        Assert.AreEqual(serialWrapper.Configuration, SerialConnectionConfiguration.Default);
     }
 }
